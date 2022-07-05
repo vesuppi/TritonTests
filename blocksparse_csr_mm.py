51c43cbc4680389c4c9f4e8106a8ad7ee24a478d
Tong Zhoou <vesuppi@fb.com>
2022-07-05 19:28:47 +0000

    To fix the block issue in blocksparse_mm

--- blocksparse_csr_mm.py
+++ blocksparse_csr_mm.py
@@ -1,9 +1,21 @@
-
+import sys
 import torch
 import triton 
 import triton.language as tl
 
-print(triton.__file__)
+class BCSR():
+    def __init__(self, rowptrs, cols, vals) -> None:
+        self.rowptrs = rowptrs
+        self.cols = cols
+        self.vals = vals
+
+
+class MCSR():
+    def __init__(self, rowptrs, cols, vals) -> None:
+        self.rowptrs = rowptrs
+        self.cols = cols
+        self.vals = vals
+
 
 def cdiv(x, y):
     return (x + y -1) // y
@@ -23,6 +35,7 @@ def gen_random_matrix(M, N, BM, BN, density=0.5, dtype=torch.float16, device='cu
     data = torch.randn([nnz, BM, BN], dtype=dtype, device=device)
     return (mask, data)
 
+
 def gen_random_matrix_dense_blocks(M, N, BM, BN, density=0.5, dtype=torch.float16, device='cuda'):
     m = cdiv(M, BM)
     n = cdiv(N, BN)
@@ -36,6 +49,35 @@ def gen_random_matrix_dense_blocks(M, N, BM, BN, density=0.5, dtype=torch.float1
                 data[i,j] = torch.zeros(BM, BN)
 
     return (mask, data)
+
+
+def gen_random_mcsr_matrix(M, N, BM, BN, density=1, dtype=torch.float16, device='cuda'):
+    m = cdiv(M, BM)
+    n = cdiv(N, BN)
+    mask = torch.zeros([m, n], dtype=torch.int, device=device)
+    data = torch.randn([m, n, BM, BN], dtype=dtype, device=device)
+    for i in range(m):
+        for j in range(n):
+            p = torch.rand(1)
+            if p[0] < density:
+                mask[i,j] = 1
+            else:
+                data[i,j] = torch.zeros(BM, BN)
+
+    nnz = torch.sum(mask)
+    rowptrs = torch.zeros(m+1, dtype=torch.int, device=device)
+    rowptrs[0] = 0
+    cols = torch.zeros(nnz, dtype=torch.int, device=device)
+    
+    nnz = 0
+    for i in range(m):
+        for j in range(n):
+            if mask[i,j] != 0:
+                cols[nnz] = j
+                nnz += 1
+        rowptrs[i+1] = nnz
+    assert nnz == torch.sum(mask)
+    return BCSR(rowptrs, cols, data)
 
 
 def gen_empty_matrix_dense_blocks(M, N, BM, BN, density=0.5, dtype=torch.float16, device='cuda'):
@@ -103,7 +145,7 @@ def test1():
 
 
 @triton.jit
-def _kernel_mm_mask_dense_block(a_mask, a_data, b_mask, b_data, c_mask, c_data, 
+def _kernel_mcsr_mm(a_rowptrs, a_cols, a_vals, b_vals, c_vals, 
                                 BM: tl.constexpr, BK: tl.constexpr, BN: tl.constexpr, 
                                 nBM: tl.constexpr, nBK: tl.constexpr, nBN: tl.constexpr,
                                 buf
@@ -112,19 +154,16 @@ def _kernel_mm_mask_dense_block(a_mask, a_data, b_mask, b_data, c_mask, c_data,
     n = tl.program_id(1)
     a_block_size = BM * BK
     b_block_size = BK * BN
-    a_ptrs = a_data + a_block_size * nBK * m + \
+    a_ptrs = a_vals + a_block_size * nBK * m + \
         tl.arange(0, BM)[:, None] * BK + tl.arange(0, BK)[None, :]
-    b_ptrs = b_data + b_block_size * n + \
+    b_ptrs = b_vals + b_block_size * n + \
         tl.arange(0, BK)[:, None] * BN + tl.arange(0, BN)[None, :]
 
-    i = 0
+    k_start = tl.load(a_rowptrs)
+    k_end = tl.load(a_rowptrs+1)
     c = tl.zeros((BM, BN), dtype=tl.float32)
-    a_mask_ptr = a_mask + m*nBK
+
     for k in range(nBK):
-        a_has_block = tl.load(a_mask_ptr)
-        a_mask_ptr += 1
-        #b_has_block = tl.load(b_mask + k*nBN + n)
-        #if a_has_block:
         a = tl.load(a_ptrs)
         b = tl.load(b_ptrs)
         c += tl.dot(a, b)
@@ -132,17 +171,26 @@ def _kernel_mm_mask_dense_block(a_mask, a_data, b_mask, b_data, c_mask, c_data,
         a_ptrs += a_block_size
         b_ptrs += b_block_size * nBN
 
+    # for kp in range(k_start, k_end):
+    #     k = tl.load(a_cols+kp)
+    #     a = tl.load(a_ptrs+a_block_size*k)
+    #     b = tl.load(b_ptrs+b_block_size * nBN*k)
+    #     c += tl.dot(a, b)
+
+    #     # a_ptrs += a_block_size
+    #     # b_ptrs += b_block_size * nBN
+
 
     c = c.to(tl.float16)
 
-    c_ptrs = c_data + (m * nBN + n) * BM * BN + \
+    c_ptrs = c_vals + (m * nBN + n) * BM * BN + \
         tl.arange(0, BM)[:, None] * BN + tl.arange(0, BN)[None, :]
     tl.store(c_ptrs, c)
 
 
-def mm_mask_dense_block(a, b, num_warps=4, num_stages=3):
-    nBM, nBK, BM, BK = a[1].shape
-    nBK, nBN, BK, BN = b[1].shape
+def mcsr_mm(a: MCSR, b: MCSR, num_warps=4, num_stages=3):
+    nBM, nBK, BM, BK = a.vals.shape
+    nBK, nBN, BK, BN = b.vals.shape
     # TODO: this does not work when M does not divide BM
     # Or maybe it works because C will also need to be padded
     M = nBM * BM 
@@ -152,30 +200,35 @@ def mm_mask_dense_block(a, b, num_warps=4, num_stages=3):
     grid = (nBM, nBN)
     #print(grid)
     buf = torch.zeros(64, device='cuda')
-    _kernel_mm_mask_dense_block[grid](a[0], a[1], b[0], b[1], c[0], c[1],
+    _kernel_mcsr_mm[grid](a.rowptrs, a.cols, a.vals, b.vals, c[1],
                                     BM, BK, BN, nBM, nBK, nBN, buf,
                                     num_warps=num_warps, num_stages=num_stages
                                     )
     return c
 
 
-def test2():
-    M = 1024
-    N = M
+def verify_run():
+    M = 32
     K = M
-    BM = 64
-    BK = 32
+    N = M
+
+    BM = 16
+    BK = BM
     BN = BM
-    a = gen_random_matrix_dense_blocks(M, K, BM, BK, density=1)
-    b = gen_random_matrix_dense_blocks(K, N, BK, BN, density=1)
-    a_ref = from_block_format(a[1])
-    b_ref = from_block_format(b[1])
+    a = gen_random_mcsr_matrix(M, K, BM, BK, density=1)
+    b = gen_random_mcsr_matrix(K, N, BK, BN, density=1)
+    a_ref = from_block_format(a.vals)
+    b_ref = from_block_format(b.vals)
     c_ref = torch.mm(a_ref, b_ref)
-    print(c_ref)
-  
-    c = mm_mask_dense_block(a, b)
+    c = mcsr_mm(a, b)
     print('verify passes:', torch.allclose(c_ref, from_block_format(c[1])))
 
+
+def benchmark_run():
+    M = 2048
+    K = M
+    N = M
+    
     BMs = [16, 32, 64]
     BKs = [16, 32, 64]
     BNs = [16, 32, 64]
@@ -185,21 +238,29 @@ def test2():
     TEST_RUN = True
 
     if TEST_RUN:
-        BMs, BKs, BNs = [32], [32], [32]
+        BMs, BKs, BNs = [32], [16], [32]
         stages, warps = [1,2,3,4,5], [1,2,4]
 
     times = []
     for BM in BMs:
         for BK in BKs:
             for BN in BNs:
+                a = gen_random_mcsr_matrix(M, K, BM, BK, density=1)
+                b = gen_random_mcsr_matrix(K, N, BK, BN, density=1)
+                a_ref = from_block_format(a.vals)
+                b_ref = from_block_format(b.vals)
+                c_ref = torch.mm(a_ref, b_ref)
+                ms, _, _ = triton.testing.do_bench(lambda: torch.mm(a_ref, b_ref))
+                print(f'torch mm: {ms:.4f}')
+                
                 for num_stages in stages:
                     for num_warps in warps:
-                        ms, _, _ = triton.testing.do_bench(lambda: mm_mask_dense_block(a, b, num_warps, num_stages))
+                        ms, _, _ = triton.testing.do_bench(lambda: mcsr_mm(a, b, num_warps, num_stages))
                         times.append((ms, BM, BK, BN, num_stages, num_warps))
-    times.sort(key=lambda x: x[0])
-    print(f'blocksparse mm: {times[0][0]:.4f}')
+                times.sort(key=lambda x: x[0])
+                print(f'blocksparse mm: {times[0][0]:.4f}')
 
-    ms, _, _ = triton.testing.do_bench(lambda: torch.mm(a_ref, b_ref))
-    print(f'torch mm: {ms:.4f}')
+    
 
-test2()
+verify_run()
+benchmark_run()
